23f3c51fd12395b05a1bef2d504583b8c8ff1c4b
polic99 <polic9280@gmail.com>
2016-11-13 18:47:00 +0900

    Digital Clock and Analog Clock
    Control Clock’s rotation using TimeSpan and DateTime

---
+++ ClockAnimator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+	public Transform hours, minutes, seconds;
+	public bool analog;
+	private const float 
+		hoursToDegrees = 360f/12,
+		minutesToDegrees = 360f/60f,
+		secondsToDegrees = 360f/60f;
+
+
+	private void Update() {
+		if (analog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			hours.localRotation =
+				Quaternion.Euler (0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+			minutes.localRotation = 
+				Quaternion.Euler (0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation = 
+				Quaternion.Euler (0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees);
+		} else {
+			DateTime time = DateTime.Now;
+			hours.localRotation = 
+				Quaternion.Euler (0f, 0f, time.Hour * -hoursToDegrees);
+			minutes.localRotation = 
+				Quaternion.Euler (0f, 0f, time.Minute * -minutesToDegrees);
+			seconds.localRotation = 
+				Quaternion.Euler (0f, 0f, time.Second * -secondsToDegrees);
+		}
+	}
+}
